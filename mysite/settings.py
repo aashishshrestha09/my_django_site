9ba92bcd3d48cf11d7b507a8ed7747dfc7fb4380
aashishshrestha09 <54453596+aashishshrestha09@users.noreply.github.com>
2020-04-18 04:50:07 -0400

    Add feature for new users to sign up

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = '1%+pu1lhg8e)33a$l=yjercbrn-_cat#p&($iw7g(j(gg1q(qa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
 ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = 'post_list'
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
